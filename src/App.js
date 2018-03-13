db492f19205251deebd313dea2c47400e2d0b7ef
Kevin <GreenApple76@users.noreply.github.com>
2018-03-13 14:31:32 -0400

    Allow user to input a new todo to local storage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 import Title from './components/Title';
 import AddToDo from './components/AddToDo';
 import ToDoList from './components/ToDoList';
+import uuid from 'uuid';
+
+// does a todo list already exist?
+if (localStorage.getItem('todoList') !== null) {
+  var todoList = JSON.parse(localStorage.getItem('todoList'));
+}
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+  // set initial state to the todoList from local storage if it exists
+  this.state = todoList ? { todoList: todoList } : { todoList: [] };
+  this.handleChange = this.handleChange.bind(this);
+  this.addTodo = this.addTodo.bind(this);
+  }
+
+  // did user type a character into the input box (if so update state)?
+  handleChange(val) {
+    console.log('change occured');
+    console.log('val: ', val);
+    this.setState({
+        value: val
+    });
+}
+
+  // add todo to the todo list, save to local storage
+  addTodo(e) {
+    e.preventDefault()
+    let todo = { id: uuid(), 
+                 todo: this.state.value
+    };
+    this.setState({ todoList: this.state.todoList.concat(todo)}, () => {
+      localStorage.setItem('todoList', JSON.stringify(this.state.todoList));
+    });
+    
+    console.log(this.state.todoList);
+  }
+
   render() {
     return (
       <div className="App">
         <Title/>
-        <AddToDo/>
+        <AddToDo addTodo={this.addTodo} handleChange={this.handleChange} state={this.state}/>
         <ToDoList/>
       </div>
     );
--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class AddToDo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+    }
+
+    
+
+  
+
     render() {
         return (
-            <input id="add-to-do" type="text" name="add-to-do"/>
+            <div>
+                <form onSubmit={this.props.addTodo}>
+                <input id="add-todo" type="text" name="task" value={this.props.state.value} onChange={(e) => { console.log('e: ', e.target.value); this.props.handleChange(e.target.value)}}/>
+                <button>+</button>
+                </form>
+            </div>
         );
     }
 }
